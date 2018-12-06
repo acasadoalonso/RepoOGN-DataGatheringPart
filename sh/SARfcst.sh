ed72188ab8e38c7386815eeb69b939a6d60adc11
Angel Casado <acasadoalonso@gmail.com>
2018-12-06 22:58:05 +0100

    “sync”

--- sh/SARfcst.sh
+++ sh/SARfcst.sh
@@ -2,5 +2,5 @@ cd /nfs/OGN/DIRdata
 python ../src/fcst.py       >>fcst$(date +%y%m%d).log
 echo $(date +%H:%M:%S)      >>fcst$(date +%y%m%d).log
 echo "============="        >>fcst$(date +%y%m%d).log
-/bin/echo '/bin/sh ~/src/fcst.sh' | at -M $(date +%H:%M)+ 6 hours
+/bin/echo '/bin/sh ~/src/SARfcst.sh' | at -M $(date +%H:%M)+ 6 hours
 cd 
