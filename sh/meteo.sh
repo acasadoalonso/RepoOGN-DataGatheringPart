ab955d67f0d5b74defac0d22880af62ebde8872f
Angel Casado <acasadoalonso@gmail.com>
2018-12-10 09:20:54 +0100

    fix processogn name option

--- sh/meteo.sh
+++
@@ -1,6 +0,0 @@
-cd /nfs/OGN/DIRdata
-echo $(date +%H:%M:%S)      >>metar$(date +%y%m%d).log
-echo "============="        >>metar$(date +%y%m%d).log
-python ../src/lemd.py       >>metar$(date +%y%m%d).log
-echo "============="        >>metar$(date +%y%m%d).log
-cd 
