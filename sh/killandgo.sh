5671fde8f9907fde2a08c3344002fbaa505118b2
Angel Casado <acasadoalonso@gmail.com>
2019-02-28 13:10:40 +0100

    delete extra files

--- sh/killandgo.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-pkill python
-sh ~/src/SARsrc/sh/SARflight_logger.sh
-ps -ef
--- sh/meteo.sh
+++
@@ -1,6 +0,0 @@
-cd /nfs/OGN/DIRdata
-echo $(date +%H:%M:%S)      >>SARmetar$(date +%y%m%d).log
-echo "============="        >>SARmetar$(date +%y%m%d).log
-python ../src/lemd.py       >>SARmetar$(date +%y%m%d).log
-echo "============="        >>SARmetar$(date +%y%m%d).log
-cd 
