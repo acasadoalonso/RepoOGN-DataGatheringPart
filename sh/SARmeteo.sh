23f61cb58f4c2e28ec7a07b3dd1246c8d5850afa
Angel Casado <acasadoalonso@gmail.com>
2019-12-05 00:54:03 +0100

    sync

--- sh/SARmeteo.sh
+++ sh/SARmeteo.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
+if [ $# = 0 ]; then
+	city='lemd'
+else
+	city=$1
+fi
+
 cd /nfs/OGN/DIRdata
 echo $(date +%H:%M:%S)                 >>SARmetar$(date +%y%m%d).log
 echo "============="                   >>SARmetar$(date +%y%m%d).log
-python3 ../src/SARsrc/SARmeteo-lemd.py >>SARmetar$(date +%y%m%d).log
+python3 ../src/SARsrc/SARmeteo-$city.py >>SARmetar$(date +%y%m%d).log
 echo "======"$(hostname)"======="      >>SARmetar$(date +%y%m%d).log
 cd 
