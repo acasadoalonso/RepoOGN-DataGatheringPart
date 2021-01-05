ff57c1e1c3f3c474cee77b2a0993652402265e88
Angel Casado <acasadoalonso@gmail.com>
2021-01-05 19:23:11 +0100

    SYNC

--- provisioning/OGNDB.sh
+++ provisioning/OGNDB.sh
@@ -1,4 +1,4 @@
 #!/bin/bash 
-echo "DROP DATABASE OGNDB if exists" | mysql OGNDB -h 172.17.0.2 -u root -pogn 
-echo "CREATE DATABASE OGNDB if not exists" | mysql OGNDB -h 172.17.0.2 -u root -pogn 
+echo "DROP DATABASE IF EXISTS OGNDB "       | mysql  -h 172.17.0.2 -u root -pogn 
+echo "CREATE DATABASE IF NOT EXISTS OGNDB " | mysql  -h 172.17.0.2 -u root -pogn 
 mysql OGNDB -h 172.17.0.2 -u root -pogn </tmp/DBschema.sql
