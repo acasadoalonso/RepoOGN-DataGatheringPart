fcb8ae9c7be857781fcd87c8cc3b17f2f81e0f71
Angel Casado <acasadoalonso@gmail.com>
2021-01-05 19:17:02 +0100

    sync

--- provisioning/GLIDERS.sh
+++ provisioning/GLIDERS.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo "DROP TABLE GLIDERS" | mysql OGNDB -h 172.17.0.2 -u root -pogn </tmp/GLIDERS.sql
+echo "DROP TABLE GLIDERS" | mysql OGNDB -h 172.17.0.2 -u root -pogn 
 mysql OGNDB -h 172.17.0.2 -u root -pogn </tmp/GLIDERS.sql
--- provisioning/OGNDB.sh
+++ provisioning/OGNDB.sh
@@ -1,1 +1,4 @@
+#!/bin/bash 
+echo "DROP DATABASE OGNDB if exists" | mysql OGNDB -h 172.17.0.2 -u root -pogn 
+echo "CREATE DATABASE OGNDB if not exists" | mysql OGNDB -h 172.17.0.2 -u root -pogn 
 mysql OGNDB -h 172.17.0.2 -u root -pogn </tmp/DBschema.sql
