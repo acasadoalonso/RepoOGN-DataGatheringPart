52e894772f3770bd32d00610ae458b11cec33a50
Angel Casado <acasadoalonso@gmail.com>
2021-10-02 18:04:15 +0200

    sync

--- dockerfiles/sarogn.start.sh
+++ dockerfiles/sarogn.start.sh
@@ -18,14 +18,14 @@ DBpath=$(echo    `grep '^DBpath '   $CONFIGDIR/SARconfig.ini` | sed 's/=//g' | s
 location=$(echo  `grep '^location_name '   $CONFIGDIR/SARconfig.ini` | sed 's/=//g' | sed 's/^location_name //g' | sed 's/ //g' )
 SCRIPT=$(readlink -f $0)
 SCRIPTPATH=`dirname $SCRIPT`
-echo $SCRIPTPATH
+#echo $SCRIPTPATH
 cd $DBpath
 echo "Starting docker container sarogn at: "$HOSTNAME $(date) 				>>SARproc.docker.log
 echo "================================================================================"	>>SARproc.docker.log
 echo "Location: "$location "City: "$city 						>>SARproc.docker.log
 if [ ! "$(docker ps -q -f name=sarogn)" ]; then
     if [ "$(docker ps -aq -f status=exited -f name=sarogn)" ]; then
-        echo "Starting an existing container sarogn ..."				>>SARproc.docker.log
+        echo "Starting an existing exited container sarogn ..."				>>SARproc.docker.log
         docker start sarogn								>>SARproc.docker.log
     else
         echo "Starting a new container sarogn ..."					>>SARproc.docker.log
@@ -38,5 +38,5 @@ fi
 docker exec -it sarogn python3 /var/www/main/SARcalsunrisesunset.py			>>SARproc.docker.log
 docker ps -a										>>SARproc.docker.log
 /bin/echo '/bin/bash ~/src/SARsrc/dockerfiles/SARidaily.docker.sh '$city | at -M $(calcelestial -n -p sun -m set -q $city )  >>SARproc.docker.log 2>&1
-echo "Container scheduled ...$(date) "							>>SARproc.docker.log
-echo "============================== "							>>SARproc.docker.log
+echo "Container scheduled ... at: $(date) "						>>SARproc.docker.log
+echo "=============================================================================== "	>>SARproc.docker.log
