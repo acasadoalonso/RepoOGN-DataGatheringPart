11531829e0f14c47b8f12a98ee15352f85ea3990
Angel Casado <acasadoalonso@gmail.com>
2024-02-05 23:38:46 +0100

    sync ... delete old sh scripts

--- sh/nfs.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-sudo service rpcbind start
-sudo mount -t nfs casadonfs:/nfs/NFS/Documents             /nfs
-sudo mount -t nfs casadoix2:/Documents                     /oldnfs
-df
-
--- sh/wlanup.sh
+++
@@ -1,4 +0,0 @@
-sudo /sbin/ifdown wlan0
-sudo /sbin/ifup   wlan0
-sleep  10
-ip a
